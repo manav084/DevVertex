0516e7fc89b02c45a2484be5cd7a0d10443a1492
Manav Pandey <manavpandey084@gmail.com>
2026-04-05 18:49:01 +0530

    Implement complete user CRUD operations including signup, fetch, update, and delete APIs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const User = require('./models/user')
 app.use(express.json())
 
 
+ // Sending a details of a user required for login after signup
 app.post("/signup", async (req,res)=>{
     
     // Creating a  new instance of te User model
@@ -19,6 +20,102 @@ app.post("/signup", async (req,res)=>{
         
     }
 
+})
+
+
+// Fetch a user by EmailID. if there are multiple  sane email id's it'll fetch only one 
+app.get("/getuser", async (req,res)=> {
+    const userEmail =  req.body.emailId ; 
+
+   try {
+      
+    const user = await User.findOne({ emailId : userEmail})
+    if(user){
+    res.send(user)
+}else{
+     res.status(404).send("Error not finding the user email")
+}
+
+
+   } catch (error) {
+
+ res.status(400).send("Error saving the User" + err.message)    
+   }
+
+
+})
+
+ // Fetch list of all users present in a database
+
+app.get("/feed", async (req,res)=> {
+    
+    
+    
+    try {
+        const users = await User.find({})
+
+        if(users.length> 0){
+
+            res.send(users)
+        }else{
+            res.status(404).send("Unable to fetch user data")
+        }
+
+        
+    } catch (error) {
+
+         res.status(400).send("Error saving the User" + error.message)    
+
+
+        
+    }
+
+})
+
+// Delete user find by id and delete
+
+app.delete("/user", async (req,res)=> {
+
+      const deleteId = req.body.deleteId
+    
+      
+      try {
+        const user = await User.findByIdAndDelete(deleteId)
+        if(user){
+        res.send("User Deleted Successfully")
+        }else{
+            res.status(404).send("User  already deleted or User does not created")
+        }
+    } catch (error) {
+
+         res.status(400).send("Error saving the User" + error.message)    
+
+    }
+})
+
+
+// Update the user using patch (patch just replace only  change part )
+
+app.patch("/user", async (req,res)=>{
+    const userId = req.body.userId 
+    const updatedData = req.body
+
+    try {
+
+         const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {new:true})
+
+         res.send({
+            message: "User updated successfully",
+            data: updatedUser })
+
+
+        
+    } catch (error) {
+
+            res.status(400).send("Error saving the User" + error.message)    
+
+        
+    }
 })
 
 connectDB().then(()=>{
